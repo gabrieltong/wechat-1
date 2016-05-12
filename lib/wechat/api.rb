abaaf59bdac865578f6afb4201c4dbd30afc8fc6
Gabriel Tong <tongyangmath@gmail.com>
2016-05-12 18:09:33 +0800

    更新we chat repo

--- lib/wechat/api.rb
+++ lib/wechat/api.rb
@@ -138,7 +138,7 @@ module Wechat
         code: code,
         grant_type: 'authorization_code'
       }
-      client.get 'sns/oauth2/access_token', params: params, base: OAUTH2_BASE
+      client.get 'oauth2/access_token', params: params, base: OAUTH2_BASE
     end
 
     def web_auth_access_token(web_access_token, openid)
@@ -151,11 +151,11 @@ module Wechat
         grant_type: 'refresh_token',
         refresh_token: user_refresh_token
       }
-      client.get 'sns/oauth2/refresh_token', params: params, base: OAUTH2_BASE
+      client.get 'oauth2/refresh_token', params: params, base: OAUTH2_BASE
     end
 
     def web_userinfo(web_access_token, openid, lang = 'zh_CN')
-      client.get 'sns/userinfo', params: { access_token: web_access_token, openid: openid, lang: lang }, base: OAUTH2_BASE
+      client.get 'userinfo', params: { access_token: web_access_token, openid: openid, lang: lang }, base: OAUTH2_BASE
     end
   end
 end
